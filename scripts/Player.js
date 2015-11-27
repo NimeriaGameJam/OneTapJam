5cd46fe5d56a37938a83e0a468582c8de564e64d
vincent-clipet <vincent.clipet.7@gmail.com>
2015-11-27 22:21:50 +0100

    ADD ::: character animation

--- scripts/Player.js
+++ scripts/Player.js
@@ -3,6 +3,24 @@ function Player(tileset, controller) {
 	this.action = Player.ACTION.STAY;
 	this.tileset = tileset;
 	this.controller = controller;
+	
+	this.animationState = 0;
+	this.animationCurrentVisualOffset = 0;
+	this.animationFrames = [ // TODO : externalize ?
+		this.tileset.get("run0"),
+		this.tileset.get("run1"),
+		this.tileset.get("run1"),
+		this.tileset.get("run2"),
+		this.tileset.get("run2"),
+		this.tileset.get("run0"),
+		this.tileset.get("run3"),
+		this.tileset.get("run3"),
+		this.tileset.get("run4"),
+		this.tileset.get("run4"),
+		this.tileset.get("run5"),
+		this.tileset.get("run5")
+	];
+	this.animationMaxState = this.animationFrames.length;
 }
 
 Player.prototype = {
@@ -11,21 +29,40 @@ Player.prototype = {
 	*/
 	update: function(world, time) {
 
-		if(this.controller.stat.press && this.controller.stat.release){
+		if (this.animationState != 0) // animation in progress
+		{
+			if (this.animationState == this.animationMaxState) // last animation frame
+			{
+				this.pos = Math.round(this.pos + 1 / this.animationMaxState);
+				this.animationState = 0;
+			}
+			else // any frame but the last one
+			{
+				this.animationState++;
+				this.pos += 1 / this.animationMaxState;
+				
+				return;
+			}
+		}
+	
+		if(this.controller.stat.press && this.controller.stat.release && this.animationState == 0){
 
 			this.action = Player.ACTION.LOAD;
 
-			var time = Date.now() - this.controller.stat.pressTime;
+			var pressTime = Date.now() - this.controller.stat.pressTime;
 
-			if(time > Game.TIME_STEP * 5){
-
+			if(pressTime > Game.TIME_STEP * 5)
+			{
 				this.pos+=2;
+				
 				this.controller.stat.press = false;
 				this.controller.stat.release = false;
 			}
-			else{
-
-				this.pos++;
+			else
+			{
+				this.animationState = 1;
+				this.pos += 1 / this.animationMaxState;
+				
 				this.controller.stat.press = false;
 				this.controller.stat.release = false;
 			}
@@ -35,18 +72,18 @@ Player.prototype = {
 	/*
 	 * Get the right texture from the tileset.
 	*/
-	getPlayer: function(time) {
-		return this.tileset.get("run0");
+	getPlayerSprite: function() {
+		return this.animationFrames[this.animationState % this.animationMaxState];
 	},
 
 	/*
 	 *Visual render.
 	*/
 	render: function(ctx, time) {
-		var pos = this.transpose(this.pos);
-		var player = this.getPlayer(time);
+		var currentPos = this.transpose(this.pos);
+		var sprite = this.getPlayerSprite();
 
-		ctx.drawImage(this.tileset.texture, player.x, player.y, player.w, player.h, pos.x-player.offX, pos.y-player.offY, player.w, player.h);
+		ctx.drawImage(this.tileset.texture, sprite.x, sprite.y, sprite.w, sprite.h, currentPos.x-sprite.offX, currentPos.y-sprite.offY, sprite.w, sprite.h);
 	},
 
 	/*
@@ -56,8 +93,8 @@ Player.prototype = {
 	transposeY: 16,
 	transpose: function(x) {
 		return {
-			x: x*this.transposeX,
-			y: -x*this.transposeY
+			x: (x * this.transposeX),
+			y: (-x * this.transposeY)
 		};
 	}
 };
